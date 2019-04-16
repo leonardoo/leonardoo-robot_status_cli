305f01467d0f49296ada1afb69741e8b700ee6fd
leonardoorozcop@gmail.com <leonardoo>
2019-04-15 22:49:33 -0500

    update the version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if sys.argv[-1] == 'tag':
 
 setup(
     name='robot_status_cli',
-    version='0.2.0',
+    version=version,
     packages=find_packages(exclude=['tests', 'tests.*']),
     url='',
     install_requires=["requests==2.13.0",
